43769b99da1fed2ad71a4a0234b64717dc812ea1
Angel Huerta <aahuerta@indracompany.com>
2022-01-13 18:35:24 -0600

    Mapping the state on the UI and handle click and change events to push customers on the state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import ReservationCard from "./components/ReservationCard";
+
+import { RootState } from "./app/store";
+import { addReservation } from "./features/reservationSlice";
+
 import "./App.css";
 
 function App() {
+  const [reservationName, setReservationName] = useState("");
+
+  const reservations = useSelector((state: RootState) => state.reservations.value);
+
+  const handleChangeReservation = (event: ChangeEvent<HTMLInputElement>) => {
+    setReservationName(event.target.value);
+  };
+
+  const dispatch = useDispatch();
+
+  const handleClickAddReservation = () => {
+    if (!reservationName) return;
+
+    dispatch(addReservation(reservationName));
+    setReservationName("");
+  };
+
   return (
     <div className="App">
       <div className="container">
@@ -9,12 +33,14 @@ function App() {
           <div>
             <h5 className="reservation-header">Reservations</h5>
             <div className="reservation-cards-container">
-              <div className="reservation-card-container">Laith Harb</div>
+              {reservations.map((name) => {
+                return <ReservationCard name={name} />
+              })}
             </div>
           </div>
           <div className="reservation-input-container">
-            <input />
-            <button>Add</button>
+            <input value={reservationName} onChange={handleChangeReservation} />
+            <button onClick={handleClickAddReservation}>Add</button>
           </div>
         </div>
         <div className="customer-food-container">
