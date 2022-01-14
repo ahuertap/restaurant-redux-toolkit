859999fde6cb756016dfb1a2c95da3466594c954
ahuerta <angeladrianhp@hotmail.com>
2022-01-14 10:03:53 -0600

    Adding customer state and creating customer slice

---
+++ src/features/customerSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Customer {
+  id: string;
+  name: string;
+  food: string[];
+};
+
+interface CustomerState {
+  value: Customer[];
+};
+
+const initialState: CustomerState = {
+  value: [],
+};
+
+export const customerSlice = createSlice({
+  name: 'customers',
+  initialState,
+  reducers: {
+    addCostumer: (state, action: PayloadAction<Customer>) => {
+      state.value.push(action.payload)
+    }
+  }
+});
+
+export const { addCostumer } = customerSlice.actions;
+
+export default customerSlice.reducer;
